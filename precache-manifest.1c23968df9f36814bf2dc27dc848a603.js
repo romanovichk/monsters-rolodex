46fb42d1015315cda5ab0e20ca7b1a7fdeb686c8
Romka <digitalkoma@Romkas-MacBook-Air.local>
2020-01-27 16:02:00 -0800

    Updates

---
+++ precache-manifest.1c23968df9f36814bf2dc27dc848a603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc90367db6d41aa893793efdb2837ab6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e89aa628de23d421e87b",
+    "url": "/monsters-rolodex/static/css/main.80865433.chunk.css"
+  },
+  {
+    "revision": "6ef49256656a9eb2de46",
+    "url": "/monsters-rolodex/static/js/2.8629efad.chunk.js"
+  },
+  {
+    "revision": "e89aa628de23d421e87b",
+    "url": "/monsters-rolodex/static/js/main.a8cfdfea.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
